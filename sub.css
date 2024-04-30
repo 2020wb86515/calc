dcd27db8e127bad6fd576ee71b7fe89016fb1f76
2020wb86515 <168257600+2020wb86515@users.noreply.github.com>
2024-04-30 09:27:45 +0000

    Create sub.css

---
+++ sub.css
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, difference;
+    
+    // Input first number
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    
+    // Input second number
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    
+    // Subtract the numbers
+    difference = num1 - num2;
+    
+    // Display the result
+    printf("Difference: %d\n", difference);
+    
+    return 0;
+}
